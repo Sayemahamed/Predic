206e0052e3c3fd147d7ff4894492995dd4695023
Sayemahamed <sayemahamed183@gmail.com>
2025-06-10 15:30:21 +0600

    testing closed source tokenizer

--- src/test.js
+++ src/test.js
@@ -1,11 +1,11 @@
 import { AutoTokenizer } from "@huggingface/transformers";
 
 // Load tokenizer for a gated repository.
-const tokenizer = await AutoTokenizer.from_pretrained(
-  "meta-llama/Llama-2-7b-hf"
-);
+const tokenizer = await AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B");
 
 // Encode text.
 const text = "Hello world!";
 const encoded = tokenizer.encode(text);
 console.log(encoded);
+const decoded = tokenizer.decode(encoded);
+console.log(decoded);
