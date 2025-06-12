fcc231bdce68e7bf80da63ff1635384d832a9089
duttaturja <duttaturja@gmail.com>
2025-06-13 01:15:03 +0600

    File Structure Fixed

--- src/model.ts
+++
@@ -1,8 +0,0 @@
-import * as ort from "onnxruntime-node"
-
-export class Model {
-    model: ort.InferenceSession
-    constructor(modelPath: string) {
-        this.model = new ort.InferenceSession(modelPath)
-    }
-}
